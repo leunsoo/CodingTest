de7ab5ace1f9e3787a2de83823beaa878549ddf9
leunsoo <98530935+leunsoo@users.noreply.github.com>
2025-02-25 17:05:41 +0900

    [Silver III] Title: 2×n 타일링, Time: 176 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		int[] arr = new int[1001];
+		
+		arr[1] = 1;
+		arr[2] = 2;
+		
+		for(int i = 3; i <= N; ++i) {
+			arr[i] = (arr[i-1]+arr[i-2])%10007;
+		}
+		
+		System.out.println(arr[N]);
+	}
+}
