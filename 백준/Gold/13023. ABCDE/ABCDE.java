ddbff8ba3db93f0eda45e1cd4385976bba2b0a81
leunsoo <98530935+leunsoo@users.noreply.github.com>
2025-02-27 11:46:08 +0900

    [Gold V] Title: ABCDE, Time: 576 ms, Memory: 22136 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	static ArrayList<ArrayList<Integer>> map = new ArrayList<ArrayList<Integer>>();
+	static boolean[] visited;
+			
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] strs = br.readLine().split(" ");
+		int N = Integer.parseInt(strs[0]); // 사람의 수
+		int M = Integer.parseInt(strs[1]); // 친구 관계의 수
+		
+		
+		for(int i = 0; i < N; ++i) {
+			map.add(new ArrayList<Integer>());
+		}
+		
+		//맵 그리기
+		for(int i = 0; i < M; ++i) {
+			strs = br.readLine().split(" ");
+			int p1 = Integer.parseInt(strs[0]);
+			int p2 = Integer.parseInt(strs[1]);
+			map.get(p1).add(p2);
+			map.get(p2).add(p1);
+		}
+		
+		for(int i = 0; i < N; ++i) {
+			visited = new boolean[N];
+			dfs(0,i);	
+		}
+		
+		System.out.println(0);
+	}
+	
+	private static void dfs(int cnt, int node) {
+		if(cnt == 5) {
+			System.out.println(1);
+			System.exit(0);
+		}
+		
+		if(map.get(node).size() == 0) return;
+		
+		for(int i = 0; i < map.get(node).size(); ++i) {
+			if(visited[map.get(node).get(i)]) continue;
+			
+			visited[map.get(node).get(i)] = true;
+			dfs(cnt+1, map.get(node).get(i));
+			visited[map.get(node).get(i)] = false;
+		}
+	}
+}
