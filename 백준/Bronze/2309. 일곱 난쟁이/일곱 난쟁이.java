9e5ddd473e0b8155777f21d45bd2fa32ada8ed7f
leunsoo <98530935+leunsoo@users.noreply.github.com>
2025-02-27 10:02:56 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 176 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -12,25 +12,32 @@ public class Main {
 		}
 		
 		Arrays.sort(origin);
-		dfs(0, 0, origin, temp, 0);
+		dfs(0, 0, origin, temp);
 		
 	}
 	
-	private static void dfs(int cnt, int idx, int[] origin, int[] temp, int sum) {
-		if(sum > 100) return;
-		if(cnt == 7 && sum == 100) {
+	private static void dfs(int cnt, int idx, int[] origin, int[] temp) {
+		if(cnt == 7) {
+			StringBuilder sb = new StringBuilder();
+			int sum = 0;
 			for (int i : temp) {
-				System.out.println(i);
+				sum += i;
+				sb.append(i).append("\n");
 			}
-			System.exit(0);
+			
+			if(sum == 100) {
+				System.out.println(sb.toString());
+				System.exit(0);
+			}
+			
+			return;
 		}
-		if(cnt == 7) return;
 		
 		for(int i = 0; i < 9; ++i) {
 			if(i < idx) continue;
 			
 			temp[cnt]= origin[i];
-			dfs(cnt+1, i+1, origin, temp,sum+ temp[cnt]);
+			dfs(cnt+1, i+1, origin, temp);
 		}
 	}
 }
