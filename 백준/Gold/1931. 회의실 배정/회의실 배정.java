ae7e108b53a971113d633f30650c377012b0dd6d
leunsoo <98530935+leunsoo@users.noreply.github.com>
2025-03-05 11:35:26 +0900

    [Gold V] Title: 회의실 배정, Time: 452 ms, Memory: 42096 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -5,19 +5,8 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-				
-		int[][] arr = new int[N][2];
 		
-		StringTokenizer stk;
-		for(int i = 0; i < N; ++i) {
-			stk = new StringTokenizer(br.readLine());
-			
-			arr[i][0] = Integer.parseInt(stk.nextToken());
-			arr[i][1] = Integer.parseInt(stk.nextToken());
-		}
-		
-		//끝나는 시간 순으로 오름차순 
-		Arrays.sort(arr, new Comparator<int[]>() {
+		PriorityQueue<int[]> pq = new PriorityQueue<>( new Comparator<int[]>() {
 			@Override
 			public int compare(int[] o1, int[] o2) {
 				if(o1[1] == o2[1]) {
@@ -27,11 +16,26 @@ public class Main {
 			}
 		});
 		
+		StringTokenizer stk;
+		for(int i = 0; i < N; ++i) {
+			stk = new StringTokenizer(br.readLine());
+			
+			int start = Integer.parseInt(stk.nextToken());
+			int end = Integer.parseInt(stk.nextToken());
+			
+			pq.add(new int[] {start, end});
+		}
+		
+
 		int answer = 1;
-		int start = arr[0][1]; // 맨처음 끝나는 시간
-		for(int i = 1; i < N; ++i) {
-			if(arr[i][0] >= start) { // 회의 끝난 후 다음 타자 교체 
-				start = arr[i][1];
+		int end = pq.poll()[1]; // 맨처음 끝나는 시간
+		
+		while (!pq.isEmpty()) {
+			int[] next = pq.poll();
+			
+			if(next[0] >= end) 
+			{
+				end = next[1];
 				answer++;
 			}
 		}
