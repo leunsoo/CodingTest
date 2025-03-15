e82004e1090978b92c28fa1da07e29e0ce4bb336
leunsoo <98530935+leunsoo@users.noreply.github.com>
2025-03-15 16:03:35 +0900

    [Gold IV] Title: 동전 1, Time: 112 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -19,18 +19,7 @@ public class Main {
 		
 		for(int i = 0; i < n; ++i) { 
 			for(int j = arr[i]; j <= k ; j++) {
-				// 기존에 계산된 동전이 있고 현재 동전을 추가할 수 있다면 
-				if(dp[j-arr[i]] != 0) { 
-					dp[j] += dp[j-arr[i]];
- 				}
-				
-				// 현재 가치가 가능한 동전이 없는데 현재 동전은 가능하다면 
-				if(dp[j] == 0) { 
-					// 현재 동전으로 게산될 수 있다면
-					if(j%arr[i] == 0)  
-						dp[j] = 1; //1로 설정 
-				}
-				
+				dp[j] += dp[j-arr[i]];
 			}
 		}
 		
