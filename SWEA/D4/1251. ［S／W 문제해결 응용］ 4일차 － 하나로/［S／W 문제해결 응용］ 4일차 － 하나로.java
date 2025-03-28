be996b2d8e26e48feb14a3139fadb286cc70f6b6
leunsoo <98530935+leunsoo@users.noreply.github.com>
2025-03-28 11:56:07 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 603 ms, Memory: 94,752 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -2,110 +2,107 @@ import java.io.*;
 import java.util.*;
 
 class Island {
-	int x, y;
-	
-	public Island(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
+    int x, y;
+    public Island(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
 
-class Edge implements Comparable<Edge> {
-	int start, to;
-	double dist;
-	
-	public Edge(int start, int to, double dist) {
-		this.start = start;
-		this.to = to;
-		this.dist = dist;
-	}
-	
-	@Override
-	public int compareTo(Edge o) {
-		// TODO Auto-generated method stub
-		return Double.compare(dist, o.dist);
-	}
+class Node implements Comparable<Node> {
+    int idx;
+    double dist;
+    
+    public Node(int idx, double dist) {
+        this.idx = idx;
+        this.dist = dist;
+    }
+    
+    // 거리가 작은 순으로 정렬
+    @Override
+    public int compareTo(Node o) {
+        return Double.compare(this.dist, o.dist);
+    }
 }
 
 public class Solution {
-	static ArrayList<Edge> lst;
-	static int[] parents;
-	static double answer;
-	static int N;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
+    static Island[] islands;
+    static int N;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        for (int tc = 1; tc <= T; tc++) {
+            sb.append("#").append(tc).append(" ");
+            
+            N = Integer.parseInt(br.readLine());
+            islands = new Island[N];
+            
+            // 섬의 x좌표 입력
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                islands[i] = new Island(Integer.parseInt(st.nextToken()), 0);
+            }
+            
+            // 섬의 y좌표 입력
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                islands[i].y = Integer.parseInt(st.nextToken());
+            }
+            double tax = Double.parseDouble(br.readLine());
+            
+            sb.append(Math.round(prim() * tax)).append("\n");
+        }
+        System.out.print(sb.toString());
+    }
+    
+    // 두 섬 사이의 거리 계산
+    private static double getDist(int x1, int x2, int y1, int y2) {
+        double dx = x1 - x2;
+        double dy = y1 - y2;
+        return dx * dx + dy * dy;
+    }
+    
+    private static double prim() {
+    	double answer = 0;
 
-		StringBuilder sb = new StringBuilder();
-		for(int tc = 1; tc <= T; ++ tc)
-		{
-			sb.append("#").append(tc).append(" ");
-			N = Integer.parseInt(br.readLine()); // 섬의 개수
+    	boolean[] visited = new boolean[N]; // 사이클 방지용, 현재 MST 집합에 속해있느냐
+    	double[] dist = new double[N]; // 해당 인덱스의 섬과 연결된 최소 터널 길이 
+    	
+    	
+    	Arrays.fill(dist, Double.MAX_VALUE);
+    	
+    	// MST 그리디의 성질을 이용할 PQ
+    	PriorityQueue<Node> pq = new PriorityQueue<>();
+    	pq.add(new Node(0, 0));
+    	dist[0] = 0;
+    	
+    	int cnt = 0;
+    	while (cnt < N) {
+			Node curr = pq.poll();
 			
-			Island[] islands = new Island[N]; // 섬
-			lst = new ArrayList<>(); // 섬들간의 간선을 담을 리스트
-			parents = new int[N]; // 유니온 파인드에 쓰일 배열
-			answer = 0; 
+			if(visited[curr.idx]) continue; // 사이클 방지 
+			visited[curr.idx] = true;
+			answer += curr.dist;
+			cnt++;
 			
-			StringTokenizer stk = new StringTokenizer(br.readLine());
-			for(int i = 0; i < N; ++i) { // 섬의 x 좌표
-				islands[i] = new Island(0, 0);
-				parents[i] = i;
-						
-				islands[i].x = Integer.parseInt(stk.nextToken());
+			//인접한 노드들 탐색
+			for(int to = 0; to < N; ++to) {
+				if(visited[to]) continue;
+				
+				double newDist = getDist(islands[curr.idx].x, islands[to].x, islands[curr.idx].y, islands[to].y);
+				
+				if(dist[to] <= newDist) continue;
+				
+				dist[to] = newDist;
+				pq.add(new Node(to, newDist));
 			}
-
-			stk = new StringTokenizer(br.readLine());
-			for(int i = 0; i < N; ++i) { // 섬의 y 좌표
-				islands[i].y = Integer.parseInt(stk.nextToken());
-			}
-			
-			for(int i = 0; i < N-1; ++i) {
-				for(int j = i+1; j < N; ++j) {
-					lst.add(new Edge(i, j, Math.pow(Math.abs(islands[i].x - islands[j].x),2)+ Math.pow(Math.abs(islands[i].y - islands[j].y),2)));
-				}
-			}
-			
-			Collections.sort(lst);
-			solution();
-			
-			double tax = Double.parseDouble(br.readLine()); // 세율
-			sb.append(Math.round(answer * tax)).append("\n");
 			
 		}
-		
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
-	
-	private static void solution() {
-		int cnt = 0;
-		
-		for (Edge e : lst) {
-			if(cnt == N-1) return; // 간선의 수는 N-1이어야 최소이다.
-			
-			if(union(e.start, e.to)) { // 사이클이 발생하지 않으면
-				answer += e.dist;
-				cnt++;
-			}
-		}
-	}
-	
-	private static int find(int x) {
-		if(parents[x] == x) return x;
-		return parents[x] = find(parents[x]);
-	}
-	
-	private static boolean union(int a, int b) {
-		int pa = find(a);
-		int pb = find(b);
-		
-		if(pa == pb) return false; // 사이클
-		
-		parents[pa] = pb; // 합병
-		return true;
-	}
+    	
+    	return answer;
+    }
+    
 }
