d57307e7156a54980a4f048e6b3378e916777f42
leunsoo <98530935+leunsoo@users.noreply.github.com>
2025-04-03 11:05:59 +0900

    [D6] Title: [Professional] 타일링, Time: 99 ms, Memory: 27,392 KB -BaekjoonHub

---
+++ SWEA/D6/4070. ［Professional］ 타일링/README.md
@@ -0,0 +1,15 @@
+# [D6] [Professional] 타일링 - 4070 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWI8A_hqZ70DFAUH) 
+
+### 성능 요약
+
+메모리: 27,392 KB, 시간: 99 ms, 코드길이: 896 Bytes
+
+### 제출 일자
+
+2025-04-03 11:05
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D6/4070. ［Professional］ 타일링/［Professional］ 타일링.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+ 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        
+        BigInteger[] dp = new BigInteger[251];
+        
+        dp[0] = BigInteger.ONE;
+        dp[1] = BigInteger.valueOf(1);
+        BigInteger two = BigInteger.valueOf(2);
+        
+        for(int i = 2; i <= 250; ++i) {
+        	dp[i] = dp[i-1];
+        	dp[i] = dp[i].add(dp[i-2].multiply(two));
+        }
+        
+        
+        StringBuilder sb = new StringBuilder();
+        for(int tc = 1; tc <= T; ++tc) {
+            sb.append("#").append(tc).append(" ").append(dp[Integer.parseInt(br.readLine())]).append("\n");
+        }   
+         
+        System.out.println(sb.toString());
+    }
+}
